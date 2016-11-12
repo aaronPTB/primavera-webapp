d8ccb17e5e6179ea87db3ce9dc0df74d3329a077
Aaron Evans <aaron.evans@utexas.edu>
2016-11-12 10:18:16 -0600

    update

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
 from flask import Flask
-from flask import render_template
 from flask import request
+from flask import render_template
 
 from primavera.primavera.primavera import primavera
 
-app = Flask(__name__, static_url_path='static')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("index.html");
 
-@app.post("/submit",methods=["POST"])
+@app.route("/submit",methods=["POST"])
 def run():
     '''takes posts to /submit and runs it through primavera'''
 
