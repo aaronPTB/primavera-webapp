369a2a46d0562378bc964a3e884f7c9e43cc4587
Aaron Evans <aaron.evans@utexas.edu>
2016-11-11 22:20:47 -0600

    update

--- server.py
+++ server.py
@@ -1,8 +1,22 @@
-import flask, render_template from flask
-import primavera from primavera
+from flask import Flask
+from flask import render_template
+from flask import request
 
-app = flask.app(__name__)
+from primavera.primavera.primavera import primavera
+
+app = Flask(__name__, static_url_path='static')
 
 @app.route('/')
-def home:
-    return 
+def home():
+    return render_template("index.html");
+
+@app.post("/submit",methods=["POST"])
+def run():
+    '''takes posts to /submit and runs it through primavera'''
+
+    error = None
+    metadata = request.form['metadata']
+    image = request.form['image']
+
+    #primavera_output = primavera({...})
+    return {primavera_output}
