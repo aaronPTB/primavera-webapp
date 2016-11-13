8b30cc14c9fa3dc5235683a30abf09ddb79127f2
Aaron Evans <aaron.evans@utexas.edu>
2016-11-13 12:26:15 -0600

    update

--- static/submit.js
+++ static/submit.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         data: myform,
         processData: false,
         contentType: false,
-        async: false,
+        //async: false,
         cache: false,
         success:function(response) {
           $("#output").attr('src', response.img)
