74b59e8cc803ecb679ab289b5391df0d7cdd9899
Aaron Evans <aaron.evans@utexas.edu>
2016-11-13 20:01:25 -0600

    update

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from flask import render_template
 from flask import jsonify
 from time import time
 from base64 import b64encode
+import os
 
 from primavera.primavera.primavera import primavera
 
@@ -52,4 +53,5 @@ def queue_run():
         data_uri_header = "data:image/png;base64,"
         data_uri_content = b64encode(img_data).decode("utf-8")
         data_uri = data_uri_header + data_uri_content
+        os.remove(file_path)
         return jsonify(**{'img': data_uri})
